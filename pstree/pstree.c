dfd846e59a2d4aca632bf749515e6271e0b2083b
tracer-nju <tracer@nju.edu.cn>
2022-06-13 22:36:35 +0800

    Linux LAPTOP-PG81ECDD 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  22:36:35 up 8 min,  0 users,  load average: 0.52, 0.58, 0.59

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,11 +2,14 @@
 #include <assert.h>
 #include <unistd.h>
 
+#define false 0
+#define true 1
+
 int main(int argc, char *argv[]) {
   int opt;
-  bool need_show_pids = false;
-  bool need_numeric_sort = false;
-  bool need_version = false;
+  int need_show_pids = false;
+  int need_numeric_sort = false;
+  int need_version = false;
   while((opt = getopt(argc,argv,"Vpn"))!= -1)
   {
       switch(opt)
