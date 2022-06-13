9001e42c874593044aeec6d353f80a3815ad7989
tracer-nju <tracer@nju.edu.cn>
2022-06-13 23:39:37 +0800

    Linux LAPTOP-PG81ECDD 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  23:39:37 up  1:11,  0 users,  load average: 0.52, 0.58, 0.59

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -5,6 +5,28 @@
 #define false 0
 #define true 1
 
+typedef struct ProcessInfo_s
+{
+    
+    int pid; //1
+    char * name;//2
+    char state;//3
+    int ppid;//4
+    long unsigned utime;//14
+    long unsigned vsize;//23
+} ProcessInfo_t;
+
+typedef struct ProcessNode_s
+{
+    
+    int child_size;
+    ProcessNode_t* children;
+    ProcessNode_t* parent;
+    ProcessInfo_t process_info;
+
+} ProcessNode_t;
+
+
 int main(int argc, char *argv[]) {
   int opt;
   int need_show_pids = false;
@@ -23,7 +45,8 @@ int main(int argc, char *argv[]) {
               need_version = true;
       }
   }
-    
+
+
   
   assert(!argv[argc]);
   return 0;
