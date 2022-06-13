ce9d9cbb95d5452a111df5832e520bfdb9ddf5b5
fatOrange <1300755523@qq.com>
2022-06-14 00:57:19 +0800

    readline split

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
 
 #define false 0
 #define true 1
@@ -27,6 +31,78 @@ typedef struct ProcessNode_s
 } ProcessNode_t;
 
 
+ProcessNode_t * build_process_tree(char * folder)
+{
+    DIR *dp;
+    struct dirent *ep;
+    dp = opendir ("/");
+    if (dp != NULL)
+    {
+        while (ep = readdir (dp))
+        {
+            switch (ep->d_type)
+            {
+            case DT_UNKNOWN:
+                printf("unknow ");
+                break;
+            case DT_REG:
+                printf("regular_file ");
+                printf("%s",ep->d_name);
+                if(strcmp(ep->d_name,"stat"))
+                {
+                    printf("%s",ep->d_name);
+                    FILE *fp;
+                    ssize_t read;
+                    char *line = NULL;
+                    size_t len = 0;
+                    while ((read = getline(&line, &len, fp)) != -1) 
+                    {
+                        printf("Retrieved line of length %zu :\n", read);
+                        printf("%s", line);                        
+                    }
+                    free(line);
+                }
+                break;
+            case DT_DIR:
+                printf("directory ");
+                printf("%s",ep->d_name);
+                if(strcmp(ep->d_name,"task"))
+                {
+                    ProcessNode_t *node = build_process_tree(ep->d_name);
+                }
+                break;
+            // case DT_FIFO:
+            //     printf("pipe ");
+            //     break;
+            // case DT_SOCK:
+            //     printf("socket ");
+            //     break;
+            // case DT_CHR:
+            //     printf("character device ");
+            //     break;
+            // case DT_BLK:
+            //     printf("block device ");
+            //     break;
+            // case DT_LNK:
+            //     printf("link device");
+            //     break;
+            default:
+                break;
+            }
+            mode_t a = IFTODT(DT_LNK);
+            printf("%d ",ep->d_reclen);
+
+            printf("\n"); 
+            // put (ep->d_off);
+            // puts ("--------------");
+        }
+        (void) closedir (dp);
+    }
+    else
+        perror ("Couldn't open the directory");
+
+}
+
 int main(int argc, char *argv[]) {
   int opt;
   int need_show_pids = false;
@@ -45,6 +121,8 @@ int main(int argc, char *argv[]) {
               need_version = true;
       }
   }
+
+
 
 
   
